b84c31f125ab5fc3f49e6bba4ad00c082a5a562c
Rose Strauss <92963227+rosestrauss1@users.noreply.github.com>
2022-02-13 08:15:42 -0800

    Add files via upload

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -17,3 +17,12 @@ lot_summary <- mecha_coil  %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PS
                                                                          Var_PSI=var(PSI),
                                                                          Std_Dev_PSI=sd(PSI),
                                                                          .groups = 'keep')
+t.test(mecha_coil$PSI,mu=1500)
+
+lot1 <- subset(mecha_coil, Manufacturing_Lot=="Lot1")
+lot2 <- subset(mecha_coil, Manufacturing_Lot=="Lot2")
+lot3 <- subset(mecha_coil, Manufacturing_Lot=="Lot3")
+
+t.test(lot1$PSI,mu=1500)
+t.test(lot2$PSI,mu=1500)
+t.test(lot3$PSI,mu=1500)
